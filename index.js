c29dc2a68442790a48c96782b3d5f788c1a83bfe
Mdjajuder <djanha723@gmail.com>
2024-08-24 10:32:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
+const userRoutes = require('./user');
 
 const app = express();
 
